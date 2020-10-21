906e31915ebc2398a3d11ae7c439fccb166250b8
quanghuy29 <73057345+quanghuy29@users.noreply.github.com>
2020-10-21 16:37:10 +0700

    Update RenderUI.java

--- src/UI/RenderUI.java
+++ src/UI/RenderUI.java
@@ -1,4 +1,78 @@
 package UI;
 
-public class RenderUI {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+
+
+import java.awt.Color;
+import java.awt.Component;
+public class RenderUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RenderUI frame = new RenderUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RenderUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 695, 576);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(10, 31, 223, 498);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel_1.setBounds(25, 10, 175, 77);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel.setBounds(25, 105, 175, 77);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_2.setBounds(25, 203, 175, 77);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_3.setBounds(25, 301, 175, 77);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("New label");
+		lblNewLabel_4.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblNewLabel_4.setBounds(25, 398, 175, 77);
+		panel.add(lblNewLabel_4);
+	}
 }
