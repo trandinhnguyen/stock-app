eb1fb08a379422876f36b436240680cf20212f0c
cysonguyen <hieungnuyenhuy20@gmail.com>
2020-11-10 15:47:56 +0700

    Create Search.java

---
+++ src/seach/Search.java
@@ -0,0 +1,87 @@
+package seach;
+
+import Model.DuLieuMaCoPhieu;
+import Model.DuLieuNhomNganh;
+import Model.DuLieuSinhCau;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Search {
+
+    private ArrayList<DuLieuMaCoPhieu>  cophieu;
+    private ArrayList<DuLieuNhomNganh> nhomnganh;
+    private String agr1, agr2, id;
+//    private ArrayList<DuLieuMaCoPhieu> ID1, name1;
+//    private ArrayList<DuLieuNhomNganh> ID2, name2;
+
+    // agr1 : key word      arg2: ID or neme    i = 0
+    public static int search1(String agr1, String agr2[], int i, int n) {
+        if (i == n && agr1[i] == agr2[i]) {
+            return 1;
+        } else if (agr1[i] == agr2[i]) {
+            return search1(agr1, agr2, i +1, n);
+        } else {
+            return 0;
+        }
+    }
+    ///  tim ID
+    public static void search2(String agr1[],ArrayList<DuLieuMaCoPhieu> arraylist1, ArrayList<DuLieuNhomNganh> arraylist2 ){
+
+        if (agr1.length == 1) {
+            for(int i = 0; i != arraylist1.size(); i++) {
+                String tmp =  arraylist1.get(i).getId();
+                if(search1(agr1, tmp, 0, agr1.length) == 1) {
+                    System.out.println(arraylist1.get(i));
+                }
+            }
+
+        }else if(agr1.length>4) {
+            for(int i = 0; i != arraylist2.size(); i++){
+                String tmp =  arraylist1.get(i).getTen();
+                if(search1(agr1, tmp, 0, agr1.length)==1) {
+                    System.out.println(arraylist2.get(i));
+                }}
+
+        }else {
+            for(int i = 0; i != arraylist1.size(); i++){
+                String tmp1 = arraylist1.get(i).getId();
+                String tmp2 = arraylist1.get(i).getTen();
+                if(search1(agr1, tmp1,0,agr1.length)==1||search1(agr1,tmp2,0,agr1.length)==1) {
+                    System.out.println(arraylist1.get(i));
+                }
+            } }
+    }
+
+    public Search() {
+        ArrayList<DuLieuSinhCau> datas = new ArrayList<DuLieuSinhCau>();
+
+        DuLieuSinhCau d = new DuLieuSinhCau("FPT", new Date(), "dfsdfsdfffffffffggggffffffffff");
+        datas.add(d);
+
+        ArrayList<String> ID1 = new ArrayList<String>();
+        ArrayList<String> name1 = new ArrayList<String>();
+//        ID2 = new ArrayList<DuLieuNhomNganh>();
+//        name2 = new ArrayList<DuLieuNhomNganh>();
+
+        ID1.add("FPT");
+        ID1.add("FPP");
+        ID1.add("FPS");
+        ID1.add("FPG");
+        name1.add("hjagsdjh");
+        name1.add("hjagssdfh");
+        name1.add("hjaaerwh");
+        name1.add("hjagsghjcxh");
+
+        String[] keyword = {"FP"};
+
+        search2(keyword, ID1, name1);
+
+    }
+
+    public static void main(String[] args){
+        new Search();
+    }
+
+}
+
