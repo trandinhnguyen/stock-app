30952556bc767382c4a1e69424e8970f40662435
cysonguyen <hieungnuyenhuy20@gmail.com>
2020-11-10 15:17:58 +0700

    Update RenderUI.java

--- src/UI/RenderUI.java
+++ src/UI/RenderUI.java
@@ -4,15 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class RenderUI extends JFrame {
+public class RenderUI extends JFrame implements ActionListener {
 
     private JPanel mainFrame;
+    private JButton tag1, tag2, tag3, tag4, tag5, search;
+    private JTextArea text;
+
 
     public RenderUI() {
         createJFrame();
     }
 
-
+    public void actionPerformed(ActionEvent e){
+        //       ActionTag action = new ActionTag();
+        if(e.getActionCommand() == tag1.getText()){
+            text.setText(null);
+            text.append("123456789hsdkashndkjasjdlkjasldkjaslkdjldskflkksdhfkjsdhlfknsldfkjlsdkjflksdjflkjsdlkfjlsdjflsdjlfkj");
+        }
+//        if(e.getActionCommand() == tag2.getText) {
+//            text.setText(null);
+//            text.append(action.getAnswer(tag2.getText()));
+//        }
+////        if(e.getActionCommand() == tag3.getText) {
+////            text.setText(null);
+////            text.append(action.getAnswer(tag3.getText()));
+////        }
+////        if(e.getActionCommand() == tag4.getText) {
+////            text.setText(null);
+////            text.append(action.getAnswer(tag4.getText()));
+////        }
+////        if(e.getActionCommand() == tag5.getText) {
+////            text.setText(null);
+////            text.append(action.getAnswer(tag5.getText()));
+////        }
+    }
 
     private void createJFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,11 +61,18 @@ public class RenderUI extends JFrame {
         panel.setBorder(BorderFactory.createEmptyBorder(15, 30, 15, 30));
         panel.setBackground(Color.green);
 
-        JButton tag1 = new JButton("tag1");
-        JButton tag2 = new JButton("tag2");
-        JButton tag3 = new JButton("tag1");
-        JButton tag4 = new JButton("tag1");
-        JButton tag5 = new JButton("tag1");
+        tag1 = new JButton("tag1");
+        tag2 = new JButton("tag2");
+        tag3 = new JButton("tag1");
+        tag4 = new JButton("tag1");
+        tag5 = new JButton("tag1");
+
+        tag1.addActionListener(this);
+        tag2.addActionListener(this);
+        tag3.addActionListener(this);
+        tag4.addActionListener(this);
+        tag5.addActionListener(this);
+
 
         panel.add(tag1);
         panel.add(tag2);
@@ -57,18 +89,28 @@ public class RenderUI extends JFrame {
         grb.gridx = 0;
         grb.gridy = 0;
         grb.ipady = 4;
-        JTextField text = new JTextField(40);
-        JButton search = new JButton("SEARCH");
+        JTextField textserch = new JTextField(40);
+        search = new JButton("SEARCH");
+
+        search.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                Actiontag action = new ActionTag;
+                text.setText("asdasdasdasdasd");
+//                text.setText(action.getAnswer(textsearch.getText()));
+            }
+        });
+
         panel.add(search, grb);
         grb.gridx = 1;
         grb.ipadx = 100;
         grb.ipady = 10;
-        panel.add(text,grb);
+        panel.add(textserch,grb);
         return panel;
     }
     private JPanel textPanel() {
         JPanel panel = new JPanel();
-        JTextArea text = new JTextArea(40,70);
+        text = new JTextArea(40,70);
         panel.add(text);
         return panel;
     }
