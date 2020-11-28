8b3c10227b9f7fb79ede05349e8e7814d027b3b4
ledoanviet <73031214+ledoanviet@users.noreply.github.com>
2020-11-28 21:28:03 +0700

    Update Search.java

--- src/seach/Search.java
+++ src/seach/Search.java
@@ -1,86 +1,34 @@
 package seach;
 
+import Model.DuLieuCongTy;
 import Model.DuLieuMaCoPhieu;
 import Model.DuLieuNhomNganh;
 import Model.DuLieuSinhCau;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Search {
-    private ArrayList<Integer> solution1;
-    private ArrayList<Integer> solution2;
-    private ArrayList<Integer> solution3;
-    private Date today= new Date();
+public abstract class  Search<T> {
+    private ArrayList<T> result;
 
-
-
-    // tim ID
-    public void search(String agr, ArrayList<DuLieuMaCoPhieu> arraylist1, ArrayList<DuLieuNhomNganh> arraylist2 ){
-        if ("VNINDEX".startsWith(agr)==true){
-            solution3.add(1);
-        }
-            if (agr.length() == 1) {
-                for (int i = 0; i != arraylist1.size(); i++) {
-                    if (arraylist1.get(i).getId().startsWith(agr) == true && arraylist1.get(i).getNgay() == today) {
-                        System.out.println(arraylist1.get(i).getId());
-                        solution1.add(i);
-                    }
-                }
-
-            } else if (agr.length() > 6) {
-                for (int i = 0; i != arraylist1.size(); i++) {
-                    if (arraylist1.get(i).getTen().startsWith(agr) == true && arraylist1.get(i).getNgay() == today) {
-                        System.out.println(arraylist1.get(i).getId());
-                        solution1.add(i);
-                    }
-
-                }
-            } else {
-                for (int i = 0; i != arraylist1.size(); i++) {
-                    if ((arraylist1.get(i).getId().startsWith(agr) == true || arraylist1.get(i).getTen().startsWith(agr) == true) && arraylist1.get(i).getNgay() == today) {
-                        System.out.println(arraylist1.get(i).getId());
-                        solution1.add(i);
-                    }
-                }
+    public Search (ArrayList<T> searchObject , String text){
+        for (T str : searchObject){
+            if (soSanh(str,text)){
+                result.add(str);
             }
-            // if( agr.length() == 1) {
-            //     for(int i = 0; i != arraylist2.size(); i++) {
-            //         if(arraylist2.get(i).getId().startsWith(agr)==true&&arraylist2.get(i).getNgay()==today) {
-            //             solution2.add(i);
-            //         }
-            //     }
-
-            // }else if(agr.length()>6) {
-            //     for(int i = 0; i != arraylist1.size(); i++){
-            //      if(arraylist2.get(i).getTen().startsWith(agr)==truearraylist2.get(i).getNgay()==today) {
-            //          solution2.add(i);
-            //     }
-            //     }
-
-            // }else {
-            //      for(int i = 0; i != arraylist2.size(); i++){
-            //          if((arraylist2.get(i).getId().startsWith(agr)==true||arraylist2.get(i).getTen().startsWith(agr)==true)&&arraylist2.get(i).getNgay()==today) {
-            //          solution2.add(i);
-            //          }
-            //      }
-            //  }
-
         }
-
-
-
-
-        public ArrayList<Integer> getSoulution1(){
-            return solution1;
-        }
-        public ArrayList<Integer> getSoulution2(){
-            return solution2;
-        }
-
-        public ArrayList<Integer> getSolution3(){
-            return solution3;
-        }
-
     }
+    public boolean soSanh(T str, String text){
+        if(str.equals(text)){
+            return true;
+        }else return false;
+    }
+
+    public abstract boolean soSanh(DuLieuMaCoPhieu str, String text);
+
+    public abstract boolean soSanh(DuLieuCongTy str, String text);
+}
+
+
+
 
 
