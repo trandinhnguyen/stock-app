cc2e44883de40c682be92c01dc3ef886e225effd
ledoanviet <73031214+ledoanviet@users.noreply.github.com>
2020-12-08 17:40:45 +0700

    Update Search.java

--- src/Search/Search.java
+++ src/Search/Search.java
@@ -3,26 +3,49 @@ package Search;
 import java.util.ArrayList;
 
 public class  Search<T> {
-    private ArrayList<T> searchArrayList;
     private ArrayList<T> result;
+    private ArrayList<Answer> cauTraLoi;
 
-    private void implementationSearch(String text) {
-        this.result.clear();
-        for(T item: this.searchArrayList) {
+
+//    private void implementationSearch(String text) {
+//        this.result.clear();
+//        for(T item: this.searchArrayList) {
+//            String resource = item.toString().toLowerCase();
+//            if(resource.indexOf(text.toLowerCase()) != -1){
+//                this.result.add(item);
+//                this.chiSoHienThi.add(resource.indexOf(text.toLowerCase()));
+//                this.cauTraLoi.add(item.toString().substring(0,item.toString().indexOf(" ",50)-1)+"...");
+//            }
+//        }
+//    }
+
+    public Search(ArrayList<T> searchArrayList,int type , String text){
+        this.result = new ArrayList<T>();
+        int i = 0;
+        for(T item: searchArrayList) {
             String resource = item.toString().toLowerCase();
-            if(resource.contains(text.toLowerCase())) this.result.add(item);
+            if(resource.indexOf(text.toLowerCase()) != -1){
+                this.result.add(item);
+                Answer a = new Answer((item.toString().substring(0,item.toString().indexOf(" ",50)-1)+"..."), type, i , resource.indexOf(text.toLowerCase()));
+                this.cauTraLoi.add(a);
+                i++;
+            }
         }
     }
 
-    public Search(ArrayList<T> searchArrayList){
-        this.result = new ArrayList<T>();
-        this.searchArrayList = searchArrayList;
-    }
-
     public ArrayList<T> getResult(String text) {
-        this.implementationSearch(text);
+//        this.implementationSearch(text);
         return result;
     }
+
+//    public ArrayList<Integer> getChiSoHienThi() {
+//        return chiSoHienThi;
+//    }
+
+    public ArrayList<Answer> getCauTraLoi() {
+        return cauTraLoi;
+    }
+
 }
 
 
